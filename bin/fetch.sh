91ab6c7b5f3f1a2b7e52b8a8bb6e52c60cc9d6c5
kitptr <h.bx3485@gmail.com>
2016-01-25 11:18:27 +0100

    content update 25.12016

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-_now=$(date +"%Y_%m_%d")
+_now=$(date +"%Y%m%d")
 
-wget -O "../raw-data/$_now" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1-120"
+wget -O "../raw-data/$_now(1-500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1-500"
+
+wget -O "../raw-data/$_now(501-1000).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=501-1000"
+
+wget -O "../raw-data/$_now(1001-1500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1001-1500"
+
+wget -O "../raw-data/$_now(1501-2000).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1501-2000"
+
+wget -O "../raw-data/$_now(2001-2500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=2001-2500"
+
+# wget -O "../raw-data/$_now(2501-3000).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=2501-3000"
