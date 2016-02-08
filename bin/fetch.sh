d7dc183e767de2f048b2252b9e868ae6f9d975ff
kitptr <h.bx3485@gmail.com>
2016-02-08 01:38:11 +0100

    content update

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -2,16 +2,28 @@
 
 _now=$(date +"%Y%m%d")
 
+#_now="20160203"
+
 # pepper
+
 # wget -O "../raw-data/$_now(1-500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1-500"
 # wget -O "../raw-data/$_now(501-1000).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=501-1000"
 # wget -O "../raw-data/$_now(1001-1500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1001-1500"
 # wget -O "../raw-data/$_now(1501-2000).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1501-2000"
 # wget -O "../raw-data/$_now(2001-2500).json" "https://web-api-pepper.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=2001-2500"
 
-# salt
+#salt
+
 wget -O "../raw-data/$_now(1-500).json" "https://web-api-salt.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1-500"
 wget -O "../raw-data/$_now(501-1000).json" "https://web-api-salt.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=501-1000"
 wget -O "../raw-data/$_now(1001-1500).json" "https://web-api-salt.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1001-1500"
 wget -O "../raw-data/$_now(1501-2000).json" "https://web-api-salt.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=1501-2000"
 wget -O "../raw-data/$_now(2001-2500).json" "https://web-api-salt.horizon.tv/oesp/api/CH/deu/web/mediagroups/feeds/lgi-ch-vod-myprime-all?byHasCurrentVod=true&sort=sortTitle&range=2001-2500"
+
+# ./disable-all-films.py
+
+./import.py "../raw-data/$_now(1-500).json" 
+./import.py "../raw-data/$_now(501-1000).json" 
+./import.py "../raw-data/$_now(1001-1500).json" 
+./import.py "../raw-data/$_now(1501-2000).json" 
+./import.py "../raw-data/$_now(2001-2500).json" 
